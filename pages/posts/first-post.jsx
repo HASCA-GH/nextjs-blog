dbc06a5b0ed279ee4251f2df66dd14b5f3f75395
Humberto Asca <hasca@ciplima.org.pe>
2023-06-11 13:16:41 -0500

    ADD: Pre-rendering and data fetching with getStaticProps.

--- pages/posts/first-post.jsx
+++ pages/posts/first-post.jsx
@@ -1,12 +1,20 @@
+import Head from "next/head"
 import Link from "next/link"
+import Layout from "../../components/Layout"
+
+
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      
       <h1>First Post page...</h1>
       <h2>
         <Link href="/">Back to Home Page</Link>
       </h2>
-    </>
+    </Layout>
 )
 }
